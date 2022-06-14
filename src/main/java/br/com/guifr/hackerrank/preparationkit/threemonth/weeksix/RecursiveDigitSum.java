94eb35e804ce47fea50bdd0e142c4854bec39994
Guilherme <guilherme.fernandes@inmetrics.com.br>
2022-06-13 23:48:30 -0300

    refactore Recursive Digit Sum

--- src/main/java/br/com/guifr/hackerrank/preparationkit/threemonth/weeksix/RecursiveDigitSum.java
+++ src/main/java/br/com/guifr/hackerrank/preparationkit/threemonth/weeksix/RecursiveDigitSum.java
@@ -1,17 +1,16 @@
 package br.com.guifr.hackerrank.preparationkit.threemonth.weeksix;
 
 public class RecursiveDigitSum {
-    public static int super_digit(String s) {
-        if(s.length() == 1) return Integer.valueOf(s);
+    public static int superDigit(String s, int k) {
+        if(s.length() == 1 && k == 1) return Integer.valueOf(s);
 
         int result = 0;
+
         for(String n : s.split("")) {
             result += Integer.valueOf(n);
         }
-        return super_digit(String.valueOf(result));
+
+        return s.length() != 1 ? superDigit(String.valueOf(result), k) : superDigit(String.valueOf(result * k), 1);
     }
 
-    public static int super_digitKTimes(String s, int k) {
-        return super_digit(String.valueOf(super_digit(s) * k));
-    }
 }
--- src/test/java/br/com/guifr/hackerrank/preparationkit/threemonth/weeksix/RecursiveDigitSumTest.java
+++ src/test/java/br/com/guifr/hackerrank/preparationkit/threemonth/weeksix/RecursiveDigitSumTest.java
@@ -10,36 +10,43 @@ class RecursiveDigitSumTest {
     @Test
     @DisplayName("Return 2 When called super_digit With '9875'")
     public void test1() {
-        int actual = RecursiveDigitSum.super_digit("9875");
+        int actual = RecursiveDigitSum.superDigit("9875", 1);
         Assertions.assertEquals(2,actual);
     }
 
     @Test
     @DisplayName("Return 8 When called super_digit With '9875987598759875'")
     public void test2() {
-        int actual = RecursiveDigitSum.super_digit("9875987598759875");
+        int actual = RecursiveDigitSum.superDigit("9875987598759875", 1);
         Assertions.assertEquals(8,actual);
     }
 
     @Test
     @DisplayName("Return 8 When called super_digit With '9875' and k=4 times")
     public void test3() {
-        int actual = RecursiveDigitSum.super_digitKTimes("9875",4);
+        int actual = RecursiveDigitSum.superDigit("9875",4);
         Assertions.assertEquals(8,actual);
     }
 
     @Test
     @DisplayName("Return 8 When called super_digit With '9875987598759875' and k=1 times")
     public void test4() {
-        int actual = RecursiveDigitSum.super_digitKTimes("9875987598759875",1);
+        int actual = RecursiveDigitSum.superDigit("9875987598759875",1);
         Assertions.assertEquals(8,actual);
     }
 
     @Test
     @DisplayName("Return 9 When called super_digit With '123' and k=3 times")
     public void test5() {
-        int actual = RecursiveDigitSum.super_digitKTimes("123",3);
+        int actual = RecursiveDigitSum.superDigit("123",3);
         Assertions.assertEquals(9,actual);
+    }
+
+    @Test
+    @DisplayName("Return 6 When called super_digit With '3' and k=5 times")
+    public void test6() {
+        int actual = RecursiveDigitSum.superDigit("3",5);
+        Assertions.assertEquals(6,actual);
     }
 
 }
